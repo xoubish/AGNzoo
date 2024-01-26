51402c4f21d24e1c8f9b041f84140a5bcc9eb903
Troy Raen <raen@ipac.caltech.edu>
2024-01-26 11:37:28 -0800

    add get_TDE_sample

--- light_curves/code_src/sample_selection.py
+++ light_curves/code_src/sample_selection.py
@@ -1,5 +1,6 @@
 import astropy.units as u
 import numpy as np
+from alerce.core import Alerce
 from astropy.coordinates import SkyCoord
 from astropy.table import Table, join, join_skycoord, unique
 from astroquery.ipac.ned import Ned
@@ -313,6 +314,30 @@ def get_graham_sample(coords, labels, verbose=1):
         labels.append('Graham 19')
     if verbose:
         print('Changing Look AGN- Graham et al: ',len(CSQ))
+
+
+def get_TDE_sample(coords, labels, *, object_ids, verbose=1):
+    """ To find and append coordinates of objects with only ZTF obj name
+
+    Parameters
+    ----------
+    coords : list of astropy skycoords
+        the coordinates of the targets for which a user wants light curves
+    labels: list of strings
+        journal articles associated with the target coordinates
+    object_ids: list of strings
+        eg., [ "ZTF18accqogs", "ZTF19aakyhxi", "ZTF19abyylzv", "ZTF19acyfpno"]
+    verbose: int
+        print out debugging info (1) or not(0)
+    """
+    alerce = Alerce()
+    objects = alerce.query_objects(oid=object_ids, format="pandas")
+    tde_coords = [SkyCoord(ra, dec, frame='icrs', unit='deg') for ra, dec in zip(objects['meanra'], objects['meandec'])]
+    tde_labels = ['ZTF-Objname' for _ in objects['meanra']]
+    coords.extend(tde_coords)
+    labels.extend(tde_labels)
+    if verbose:
+        print('number of ztf coords added by Objectname:', len(objects['meanra']))
 
 
 #SDSS QSO sample of any desired number
